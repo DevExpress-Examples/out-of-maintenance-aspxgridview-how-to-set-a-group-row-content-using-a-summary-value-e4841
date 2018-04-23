89f535681ec045d9743cb599e04f47ee7273cd18
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/Default.aspx.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using DevExpress.Web.ASPxGridView;
+
+public partial class _Default : System.Web.UI.Page
+{
+    protected void Page_Init(object sender, EventArgs e)
+    {
+        ASPxGridView1.DataSource = Enumerable.Range(0, 10).Select(i => new { ID = i, Text =  i%2 == 0 ? "Group 1" : "Group 2"});
+        if (!IsPostBack)
+            ASPxGridView1.DataBind();
+        ASPxGridView1.ExpandAll();
+    }
+
+    public string GetSummaryValue(GridViewGroupRowTemplateContainer c) {
+        var grid = c.Grid;
+        var summary = grid.GroupSummary["ID"];
+        var sumValue = grid.GetGroupSummaryValue(c.VisibleIndex, summary);
+        return sumValue.ToString();
+    }
+}
---
+++ Readme.md
@@ -0,0 +1,8 @@
+# ASPxGridView - How to set a group row content using a summary value
+
+
+<p>This example demonstrates how to set a group row content using a summary value.</p>
+
+<br/>
+
+
